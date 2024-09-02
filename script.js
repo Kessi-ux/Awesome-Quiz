2da14b67e2b133fb5c96538c42b03fba34e696eb
Kessi-ux <chukwubueze.uchenyi.231361@unn.edu.ng>
2024-09-02 14:31:30 -0700

    js with lil bug around coloring of options

--- script.js
+++ script.js
@@ -26,10 +26,15 @@ start_btn.onclick = () => {
      quiz_box.classList.add("activeQuiz"); //show the quiz box
      showQuestions(0);
      queCounter(1);
+     startTimer(15);
+     startTimerLine(0);
  }
 
 let que_count = 0;
 let que_numb = 1;
+let counter;
+let timeValue = 15;
+let widthValue = 0;
 
 const next_btn = quiz_box.querySelector(".next_btn");
 
@@ -40,6 +45,10 @@ next_btn.onclick = ()=> {
         que_numb++;
         showQuestions(que_count);
         queCounter(que_numb)
+        clearInterval(counter);
+        startTimer(timeValue);
+        clearInterval(counterLine);
+        startTimerLine(widthValue);
        }else{
         console.log("Questions completed")
        }
@@ -48,45 +57,86 @@ next_btn.onclick = ()=> {
 //getting questions and options from array
 function showQuestions(index){
     let que_text = document.querySelector(".que_text");
-    let option_list = document.querySelector(".option_list");
+    // let option_list = document.querySelector(".option_list");
     let que_tag = '<span>' + questions[index].numb + ". " + questions[index].question +'</span>';
     let option_tag = '<div class="option"><span>' + questions[index].options[0] +'</span></div>'
                      +'<div class="option"><span>' + questions[index].options[1] +'</span></div>'
-                     +'<div class="option"><span>' + questions[index].options[2] +'</span></div>'
+                      +'<div class="option"><span>' + questions[index].options[2] +'</span></div>'
                      +'<div class="option"><span>' + questions[index].options[3] +'</span></div>'
     que_text.innerHTML = que_tag;
     option_list.innerHTML = option_tag;
 
-    
+    const option = option_list.querySelectorAll(".option");
+    for(let i = 0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
 }
+
+let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+function optionSelected(answer){
+    clearInterval(counter);
+    clearInterval(counterLine);
+    let userAns = answer.textContent;
+    let correctAns = questions[que_count].answer;
+    let allOptions = option_list.children.length;
+    if (userAns == correctAns){
+        answer.classList.add("correct");
+        answer.insertAdjacentHTML("beforeend", tickIcon);
+        console.log("Answer is Correct");
+    }else{
+        answer.classList.add("incorrect");
+        answer.insertAdjacentHTML("beforeend", crossIcon)
+        console.log("Answer is wrong");
+
+    //if answer is incorrect then automatically select the correct answer
+        for (let i = 0; i < allOptions; i++){
+            if(option_list.children[i].textContent == correctAns){
+                option_list.children[i].setAttribute("class", "option correct");
+                answer.insertAdjacentHTML("beforeend", tickIcon)
+             }
+        }
+    }
+
+    //once user selected disabled all options
+        for(let i = 0; i < allOptions; i++) {
+            option_list.children[i].classList.add("disabled");
+        // const element = array[index];
+    }
+     next_btn.style.display = "block";
+}
+
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time;
+        time--;
+        if(time < 9){
+            let addZero = timeCount.textContent;
+            timeCount.textContent ="0" + addZero
+        }
+        if (time < 0) { // Add this condition
+            clearInterval(counter);
+            timeCount.textContent = "00"; // Set to 0 when time is up
+        }
+    }
+}
+
+function startTimerLine(time){
+    counterLine = setInterval(timer, 29);
+    function timer(){
+        time += 1;
+        timeLine.style.width = time + "px";
+        if (time > 549) {
+            clearInterval(counterLine);
+        }
+    }
+}
+
 
 function queCounter(index){
     const bottom_ques_counter = quiz_box.querySelector(".total_que");
     let totalQuesCountTag = '<span><p>' + index + '</p>of<p>' + questions.length + '</p>Questions</span>'
     bottom_ques_counter.innerHTML = totalQuesCountTag;
 }
-
-//'<span><p>'+ index + '</p></span>' + questions.length + '</p> Questions</span>';
-// let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
-// let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
-
-// function optionSelected(answer){
-//     clearInterval(counter);
-//     clearInterval(counterLine);
-//     let userAns = answer.textContent;
-//     let correctAns = questions[que_count].answer;
-//     let allOptions = option_list.children.length;
-//     if(userAns == correctAns){
-//         userScore += 1;
-//         console.log(userScore);
-//         answer.classList.add("correct");
-//         console.log("Answer is Correct");
-//         answer.insertAdjacentHTML("beforeend", tickIcon);
-//     }else{
-//         answer.classList.add("incorrect");
-//         console.log("Answer is wrong");
-//         answer.insertAdjacentHTML("beforeend", crossIcon);
-  
-//     //if answers is incorrect than automatically selected the correct answer
-//     }
-// }
